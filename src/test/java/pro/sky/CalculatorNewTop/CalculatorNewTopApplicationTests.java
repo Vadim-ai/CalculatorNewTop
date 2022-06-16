023eba871b7120c38a1da885f4be008ece64d364
SkyPro <vadim.phobia@gmail.com>
2022-06-16 23:17:47 +0300

    Выполнил Д/З

---
+++ src/test/java/pro/sky/CalculatorNewTop/CalculatorNewTopApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.CalculatorNewTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorNewTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
